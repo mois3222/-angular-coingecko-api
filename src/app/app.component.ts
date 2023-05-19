e8ddc38a7527b17576d63556a841ad7789dc7336
mois3222 <moisky222@gmail.com>
2023-05-19 14:28:27 -0500

    config dev with bootstrap css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-coingecko-api';
+export class AppComponent implements OnInit {
+  coins: Coin[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  //it's executed when, the load page
+  ngOnInit() {
+    //get api requests
+    this.http
+      .get<Coin[]>(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.coins = res;
+        },
+        (err) => console.log(err)
+      );
+  }
 }
